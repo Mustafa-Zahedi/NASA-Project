eedd18071bba1b64d7d6a570d64447f93a040093
Mustafa Zahedi <96369008+Mustafa-Zahedi@users.noreply.github.com>
2022-09-30 17:13:55 +0430

    Front End served for production

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -5,7 +6,14 @@ const planetsRouter = require("./routes/planets/planets.router");
 const app = express();
 
 app.use(cors({ origin: "http://localhost:3000" }));
+// app.get("/", express.static(path.join(__dirname, "..", "public")));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
 module.exports = app;
